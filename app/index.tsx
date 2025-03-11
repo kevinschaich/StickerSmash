2147812af39ca4158e6ad5dc18a536ccf06aa313
kevinschaich <schaich.kevin@gmail.com>
2025-03-11 14:19:10 -0400

    Create your first app

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Home screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    // doesn't work in React Native
+    // fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    color: '#fff',
+  },
+});
